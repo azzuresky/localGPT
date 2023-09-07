a9ca37ca9c2a8dd1cffa49e546165f14c887e1f2
azzuresky <azzuresky999@gmail.com>
2023-09-07 19:44:07 +0700

    Create run_PAPERSPACE.py

---
+++ run_PAPERSPACE.py
@@ -0,0 +1,23 @@
+import subprocess
+
+# Define the paths to your Python scripts
+script1_path = 'run_localGPT_API-NGROK.py'
+script2_path = 'localGPTUI/localGPTUI-NGROK.py'
+
+# Run script1.py in a separate process
+process1 = subprocess.Popen(['python', script1_path])
+
+# Run script2.py in a separate process
+process2 = subprocess.Popen(['python', script2_path])
+
+process3 = subprocess.Popen(['lt', '--port 5111 --subdomain wysk-ui-dev'])
+
+process4 = subprocess.Popen(['lt', '--port 5110 --subdomain wysk-api-dev'])
+
+# Wait for both processes to finish
+process1.wait()
+process2.wait()
+process3.wait()
+process4.wait()
+
+print("Both scripts have finished running.")
