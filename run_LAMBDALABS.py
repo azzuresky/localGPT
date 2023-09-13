fca22a2f81e195a4a9e26f389c7cb767bec6f42c
azzuresky <azzuresky999@gmail.com>
2023-09-13 17:22:54 +0700

    Create run_LAMBDALABS.py

---
+++ run_LAMBDALABS.py
@@ -0,0 +1,18 @@
+import subprocess
+
+# Define the paths to your Python scripts
+script1_path = 'run_localGPT_API-LAMBDALABS.py'
+script2_path = 'localGPTUI/localGPTUI-LAMBDALABS.py'
+
+# Run script1.py in a separate process
+process1 = subprocess.Popen(['python', script1_path])
+
+# Run script2.py in a separate process
+process2 = subprocess.Popen(['python', script2_path])
+
+
+# Wait for both processes to finish
+process1.wait()
+process2.wait()
+
+print("All scripts have finished running.")
