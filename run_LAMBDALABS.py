e4bfd2073fe3852b1343b5ab65fdd52fca747077
azzuresky <azzuresky999@gmail.com>
2023-09-19 21:28:22 +0700

    Update run_LAMBDALABS.py

--- run_LAMBDALABS.py
+++ run_LAMBDALABS.py
@@ -4,6 +4,8 @@ import subprocess
 script1_path = 'run_localGPT_API-LAMBDALABS.py'
 script2_path = 'localGPTUI/localGPTUI-LAMBDALABS.py'
 
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32
+
 # Run script1.py in a separate process
 process1 = subprocess.Popen(['python', script1_path])
 
